f1c7d6456d750dac599dc7f7ed590ab8060e066b
burnyshev <pavel.burnysheff@yandex.ru>
2021-10-25 12:59:10 +0000

    fix evaluation

--- bin/run_evaluation.sh
+++ bin/run_evaluation.sh
@@ -3,7 +3,7 @@ MODE=eval-completion
 OUTPUT_DIR="wikitext-103-bpe_v0/checkpoints"
 
 
-for method in "mle" "ut" "iut" "alpha_entmax"; do
+for method in "mle" "ut" "iut"; do
     echo $method
     for i in 1,0.0 3,0.0 8,0.0 0,0.3 0,0.9; do
         IFS=',' read item1 item2 <<< "${i}"
@@ -13,10 +13,21 @@ for method in "mle" "ut" "iut" "alpha_entmax"; do
         
         python src/run_gpt2.py \
             --mode ${MODE} \
-            --model-load-dir ${OUTPUT_DIR}/${method}/best \
+            --model-load-dir ${OUTPUT_DIR}/${method}/best/pytorch_model.bin \
             --output-dir ${OUTPUT_DIR}/${method} \
             --top-k ${topk} \
             --top-p ${topp}
         
     done
 done
+
+
+for method in "alpha_entmax"; do
+    echo $method
+    python src/run_gpt2.py \
+        --mode ${MODE} \
+        --model-load-dir ${OUTPUT_DIR}/${method}/best/pytorch_model.bin \
+        --output-dir ${OUTPUT_DIR}/${method} \
+        --alpha-entmax
+done
+
